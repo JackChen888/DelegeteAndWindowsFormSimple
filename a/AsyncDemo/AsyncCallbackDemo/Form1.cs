a193fa4def4b9dd1b5bfaea4fb44d7e3b3561447
JackChen888 <xietg@163.com>
2018-09-06 20:58:40 +0800

    委托异步调用和委托回调函数

---
+++ a/AsyncDemo/AsyncCallbackDemo/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsyncCallbackDemo
+{
+    public partial class Form1 : Form
+    {
+        //异步回调
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.objMyCal = new MyCalculator(ExecuteTask);
+        }
+
+        //2根据委托定义方法
+        private int ExecuteTask(int num, int ms)
+        {
+            System.Threading.Thread.Sleep(ms);
+            return num * num;
+
+        }
+        MyCalculator objMyCal = null;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+          Random c = new Random();
+            for (int i = 0; i < 11; i++)
+            {
+                
+                int num = c.Next(10);
+                objMyCal.BeginInvoke(10 * i, 1000 * num, MyCallBack, i);
+            }
+        }
+
+        private void MyCallBack(IAsyncResult result)
+        {
+            int res = objMyCal.EndInvoke(result);
+
+            //异步显示结果
+            Console.WriteLine("第{0}个计算结果为：{1}", result.AsyncState, res.ToString());
+
+        }
+
+    }
+
+    //1声明一个委托
+    public delegate int MyCalculator(int num,int ms);
+
+
+}
---
+++ a/AsyncDemo/AsyncDemo/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsyncDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label3.Text = ExectueTask1(10).ToString();
+            label4.Text = ExectueTask2(10).ToString();
+
+        }
+
+        private int ExectueTask1(int num)
+        {
+            System.Threading.Thread.Sleep(5000);
+            return num * num;
+        }
+
+        private int ExectueTask2(int num)
+        {
+            return num * num;
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MyCalculator objMyCal = ExectueTask1;//创建委托变量，并代表方法1
+
+            //通过委托异步调用任务
+            //委托类型的BeginInvoke（<输入和输出变量>， AsyncCallback callback，objeck asyncState）方法：异步调用的核心
+            //第一个参数10，表示委托对应的实参
+            //第二个参数callback：回调函数，表示异步调用后自动调用的函数
+            //第三个参数asyncStatus，用于向回调函数提供参数信息
+            //返回值：IAsuncResult->异步操作状态接口，封装了异步执行中的参数
+
+
+            IAsyncResult result = objMyCal.BeginInvoke(10, null, null);
+            this.label3.Text = "正在计算请稍等...";
+
+            //同时执行其他任务
+            this.label4.Text = ExectueTask2(5).ToString();
+
+            //获取异步执行的结果
+            int res = objMyCal.EndInvoke(result);
+            this.label3.Text = res.ToString();
+        }
+
+
+        //[1]定义委托
+        public delegate int MyCalculator(int num);
+
+
+    }
+}
